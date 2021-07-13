42c1c228bf15d89121cb38b330d06ce17f558165
Otto <36742501+0x464e@users.noreply.github.com>
2021-07-13 07:09:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Tiktok To Discord
+A Discord bot to automatically download and post the raw video files behind a TikTok link.  
+Discord doesn't embed TikTok video links at all, this removes the huge annoyance of having to open the link in your web browser to view the video.  
+![Demonstration](https://i.imgur.com/k4DlynO.gif)  
+This is my first *larger* js/nodejs project, so please excuse possible bad code/implementations.
+## Getting  Started
+
+### Dependencies
+
+* [NodeJS](https://nodejs.org/en/)
+* [tiktok-scraper](https://github.com/drawrowfly/tiktok-scraper)  
+(TikTok doesn't offer an API or any official way to get videos, so this project relies on this awesome scraper being maintained)
+
+### Installing
+
+* Install [NodeJS](https://nodejs.org/en/) via whatever method is appropriate for your platform.
+* Clone this repository
+`git clone https://github.com/0x464e/tiktok_to_discord`
+* Insert your Discord bot's token into `config.json` 
+* Install the required Node packages from `package.json` by running `npm install`
+
+### Executing the application
+
+* Run `node index.js` and enjoy.
+
+### Usage
+The bot parses links from any message it can see, if TikTok link(s) are found, up to `config.MAX_TIKTOKS_PER_MESSAGE` TikToks are attempted to be downloaded.  
+Information about the usage is stored to `config.DB_PATH` json file, if not disabled via `config.USE_DATABASE`.  
+Each user receives a `config.COOLDOWN_PER_USER` ms cooldown after attempting to download a TikTok.
+
+### Maintenance
+As mentioned above, there is no official way to download TikToks, so the method used to download may break at any time. In such event, keep your eyes peeled at [tiktok-scraper](https://github.com/drawrowfly/tiktok-scraper) and update the node package after the scraper receives an update.
+If the scraper's update requires something to be rewritten in `index.js`, pull the changes from here after I make them (assuming I haven't abandoned this project by then) or fix it yourself and submit a pull request. I'll be happy to accept any reasonable PRs.
---
+++ index.js
@@ -0,0 +1,103 @@
+const config = require('./config.json');
+const TikTokScraper = require('tiktok-scraper');
+const Discord = require('discord.js');
+require('discord-reply');
+const client = new Discord.Client();
+const urlRegex = require('url-regex');
+const axios = require('axios');
+const fs = require('fs');
+const nodeCleanup = require('node-cleanup');
+const cron = require('node-cron');
+
+let cooldown_users = new Set();
+let database = fs.existsSync(config.DB_PATH) ? JSON.parse(fs.readFileSync(config.DB_PATH).toString()) : {};
+
+client.on('message', async msg => {
+    if (!msg.content || msg.author.bot || cooldown_users.has(msg.author.id))
+        return;
+
+    let found_match = false;
+    //convert to set to remove duplicates and then back to array to be able to slice (slicing so max 5 tiktoks per message)
+    Array.from(new Set(msg.content.match(urlRegex()))).slice(0, config.MAX_TIKTOKS_PER_MESSAGE).forEach((url) => {
+        if(/(www\.tiktok\.com)|(vm\.tiktok\.com)/.test(url)) {
+            cooldown_users.add(msg.author.id);
+            found_match = true;
+            try {
+                msg.channel.startTyping().then(msg.channel.stopTyping());
+            }
+            catch (e) {
+               console.log(e);
+            }
+            TikTokScraper.getVideoMeta(url).then(tt_response =>
+                axios.get(tt_response.collector[0].videoUrl, {responseType: 'arraybuffer', headers: tt_response.headers}).then(axios_response =>
+                    msg.lineReplyNoMention('', {files: [{attachment: axios_response.data, name: `${tt_response.collector[0].id}.mp4`}]}).then(update_database(msg, tt_response))
+                        .catch(console.error))                  // if sending of the Discord message itself failed, just log error to console
+                        .catch(err => report_error(msg, err)))  // if TikTokScraper.getVideoMeta() failed
+                        .catch(err => report_error(msg, err));  // if axios.get() failed
+        }
+    });
+
+    // very basic cooldown implementation to combat spam.
+    // removes user id from set after cooldown_per_user ms.
+    if(found_match)
+         (async (id = msg.author.id) => {
+            await new Promise(x => setTimeout(x, config.COOLDOWN_PER_USER));
+            cooldown_users.delete(id);
+        })();
+})
+
+async function update_database(msg, tt_response) {
+    if (!config.USE_DATABASE)
+        return
+    if (database.hasOwnProperty(msg.author.id)) {
+        const userId = msg.author.id;
+        const tt = tt_response.collector[0];
+        if (database[userId]['downloads'].hasOwnProperty(tt.id))
+            database[userId]['downloads'][tt.id]['count']++;
+        else {
+            let thumbnail = config.STORE_THUMBNAILS ? 'data:image/png;base64,' + Buffer.from((await axios.get(tt.imageUrl, {responseType: 'arraybuffer', headers: tt_response.headers}))
+                .data, 'binary').toString('base64') : "";
+            database[userId]['downloads'][tt.id] = {
+                'count': 1,
+                'description': tt.text,
+                'userName': '@' + tt.authorMeta.name,
+                'nickname': tt.authorMeta.nickName,
+                'thumbnail': thumbnail
+            };
+        }
+    }
+    else {
+        const userId = msg.author.id;
+        const tt = tt_response.collector[0];
+        let thumbnail = config.STORE_THUMBNAILS ? 'data:image/png;base64,' + Buffer.from((await axios.get(tt.imageUrl, {responseType: 'arraybuffer', headers: tt_response.headers}))
+            .data, 'binary').toString('base64') : "";
+        database[userId] = {
+            'username': msg.author.tag,
+            'firstDownload': Date.now(),
+            'downloads': {
+                [tt.id]: {
+                    'count': 1,
+                    'description': tt.text,
+                    'userName': '@' + tt.authorMeta.name,
+                    'nickname': tt.authorMeta.nickName,
+                    'thumbnail': thumbnail
+                }
+            }
+        };
+    }
+}
+
+//write database to file every hour
+cron.schedule('0 * * * *', () =>
+    fs.writeFileSync(config.DB_PATH, JSON.stringify(database)));
+
+//write database to file on any exit reason
+nodeCleanup((exitCode, signal) =>
+    fs.writeFileSync(config.DB_PATH, JSON.stringify(database)));
+
+function report_error(msg, error) {
+    msg.lineReplyNoMention(`Error on trying to download this TikTok:\n\`${error}\``).catch(console.error);
+}
+
+
+client.login(config.TOKEN).then(() => console.log('Connected as ' + client.user.tag)).catch(console.error);
