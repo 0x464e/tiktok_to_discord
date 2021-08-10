d5bba3eeb6d17df95575795f63f07b00f2b2780f
0x464e <36742501+0x464e@users.noreply.github.com>
2021-08-11 02:27:16 +0300

    Add check for filesize before uploading

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config.json');
 const TikTokScraper = require('tiktok-scraper');
-const {Intents, Client } = require('discord.js');
+const { Intents, Client } = require('discord.js');
 const client = new Client({intents:[Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const urlRegex = require('url-regex');
 const axios = require('axios');
@@ -8,6 +8,11 @@ const fs = require('fs');
 const nodeCleanup = require('node-cleanup');
 const cron = require('node-cron');
 const { execFile } = require('child_process');
+const filesizeLimit = {
+    default: 8 * 1024 * 1024 - 1000, // reserve 1KB for the message body
+    tier2: 50 * 1024 * 1024 - 1000,
+    tier3: 100 * 1024 * 1024 - 1000
+};
 
 let cooldown_users = new Set();
 let database = fs.existsSync(config.DB_PATH) ? JSON.parse(fs.readFileSync(config.DB_PATH).toString()) : {};
@@ -16,6 +21,7 @@ client.on('messageCreate', async msg => {
     if (!msg.content || msg.author.bot || cooldown_users.has(msg.author.id))
         return;
     let found_match = false;
+
     //convert to set to remove duplicates and then back to array to be able to slice (slicing so max 5 tiktoks per message)
     Array.from(new Set(msg.content.match(urlRegex()))).slice(0, config.MAX_TIKTOKS_PER_MESSAGE).forEach((url) => {
         if (/(www\.tiktok\.com)|(vm\.tiktok\.com)/.test(url)) {
@@ -28,9 +34,14 @@ client.on('messageCreate', async msg => {
                console.log(e);
             }
             TikTokScraper.getVideoMeta(url).then(tt_response =>
-                axios.get(tt_response.collector[0].videoUrl, {responseType: 'arraybuffer', headers: tt_response.headers}).then(axios_response =>
+                axios.get(tt_response.collector[0].videoUrl, {responseType: 'arraybuffer', headers: tt_response.headers}).then(axios_response => {
+                    if (is_too_large_attachment(msg, axios_response)) {
+                        msg.reply({content: 'This TikTok exceeds the file size limit Discord allows :*(', allowedMentions: {repliedUser: false}}).catch(console.error);
+                        return;
+                    }
                     msg.reply({files: [{attachment: axios_response.data, name: `${tt_response.collector[0].id}.mp4`}], allowedMentions: {repliedUser: false}}).then(update_database(msg, tt_response))
-                        .catch(console.error))                  // if sending of the Discord message itself failed, just log error to console
+                        .catch(console.error) // if sending of the Discord message itself failed, just log error to console
+                    })
                         .catch(err => report_error(msg, err)))  // if TikTokScraper.getVideoMeta() failed
                         .catch(err => report_error(msg, err));  // if axios.get() failed
         }
@@ -53,6 +64,27 @@ client.on('messageCreate', async msg => {
             cooldown_users.delete(id);
         })();
 })
+
+function is_too_large_attachment(msg, stream) {
+    let limit = 0;
+    if (!msg.guild)
+        limit = filesizeLimit.default;
+    else {
+        switch (msg.guild.premiumTier) {
+            case 'NONE':
+            case 'TIER_1':
+                limit = filesizeLimit.default;
+                break;
+            case 'TIER_2':
+                limit = filesizeLimit.tier2;
+                break;
+            case 'TIER_3':
+                limit = filesizeLimit.tier3;
+                break;
+        }
+    }
+    return stream.data.length >= limit;
+}
 
 async function update_database(msg, tt_response) {
     if (!config.USE_DATABASE)
